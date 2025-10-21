f16e62810bb17ee6cbfba8e741aa6fc2dd237557
stonec-1122 <kyle951122@gmail.com>
2025-10-21 16:39:37 +0800

    Update 2.25.cpp

--- 2.25/2.25/2.25.cpp
+++ 2.25/2.25/2.25.cpp
@@ -3,25 +3,16 @@
 
 int main()
 {
+    printf("LLLLLLLLLLL\n");
     printf("L\n");
     printf("L\n");
     printf("L\n");
-    printf("L\n");
-    printf("L      \n");
-    printf("LLLLLLLL\n");
-    printf("  CCCCCC\n");
-    printf(" CC\n");
-    printf("C\n");
-    printf("C\n");
-    printf(" CC\n");
-    printf("  CCCCCC\n");
-    printf("K     K\n");
-    printf("K    K\n");
-    printf("K   K\n");
-    printf("K K\n");
-    printf("K   K \n");
-    printf("K    K \n");
-    printf("K      K  \n");
-
-
+    printf("  CCCCCCC\n");
+    printf(" CC      CC\n");
+    printf("CC        CC\n");
+    printf("C          C\n");
+    printf("KKKKKKKKKKKK\n");
+    printf("     KK\n");
+    printf("   K    K\n");
+    printf("K          K\n");
 }
